d3317580ec69894f76e342170034f8894fdfcbf4
kcast03 <karlacastello48@gmail.com>
2023-08-29 22:00:44 -0500

    updated service layer

--- gamestore/src/main/java/com/company/gamestore/service/ServiceLayer.java
+++ gamestore/src/main/java/com/company/gamestore/service/ServiceLayer.java
@@ -1,4 +1,36 @@
 package com.company.gamestore.service;
 
-public class ServiceLayer {
+import com.company.gamestore.model.ProcessingFee;
+import com.company.gamestore.repository.ProcessingFeeRepository;
+import com.company.gamestore.repository.TaxRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ServiceLayer
+{
+    //private GameRepository gameRepository;
+    //private ConsoleRepository consoleRepository;
+    //private TshirtRepository tshirtRepository;
+    //private InvoiceRepository invoiceRepository;
+    private ProcessingFeeRepository processingFeeRepository;
+    private TaxRepository taxRepository;
+
+    @Autowired
+    public ServiceLayer(ProcessingFeeRepository processingFeeRepository, TaxRepository taxRepository){
+        this.processingFeeRepository = processingFeeRepository;
+        this.taxRepository = taxRepository;
+    }
+
+
+    // Console API
+
+
+    // Game API
+
+
+    // Tshirt API
+
+
+    //Invoice API
 }
