035a1ce21bc83904a3abcd697e86b4c66eb75924
t1fffany <102014820+t1fffany@users.noreply.github.com>
2023-08-31 15:08:39 -0700

    Quickly commiting(No need to interact with this commit)

---
+++ gamestore/src/main/java/com/company/gamestore/controller/GraphController.java
@@ -0,0 +1,20 @@
+package com.company.gamestore.controller;
+
+public class GraphController {
+
+    //Get all Games
+    //
+    //Get a Game by ID
+    //
+    //Get a Game by Title
+    //
+    //Get a Game by ESRB rating
+    //
+    //Get a Game by Studio
+    //
+    //Get all Consoles
+    //
+    //Get a Console by ID
+    //
+    //Get a Console by Manufacturer
+}
